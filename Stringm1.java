7d2be8ec81c8801e441e35b45f90652fd26b95a2
jagadeesh9200 <kodelajagadeesh@gmail.com>
2017-10-27 21:11:40 +0530

    Add files via upload

---
+++ Stringm1.java
@@ -0,0 +1,17 @@
+/*In this program to tell about the string method().
+the first method is charAt()
+syntax is char charAt(int index)
+"int index" starts with 0 1 2 3 4 5 6 like that*/
+package my;
+
+public class Stringm1 {
+
+	public static void main(String args[]) {
+	      String s = "This is a string method program";
+	      char result = s.charAt(18);
+	      System.out.println(result);
+	   }
+
+	}
+
+
